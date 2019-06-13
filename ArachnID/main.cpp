d2419f16aa8de8dca0e243f56e0911b2480b9191
gtaumaturgo <gtaumaturgo@github.com>
2019-06-12 21:41:32 -0300

    a lot of stuff

--- ArachnID/main.cpp
+++ ArachnID/main.cpp
@@ -1,11 +1,53 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include "pthread.h"
+#include "server.h"
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[])
-{
+#define MSQID 140140522
+
+pthread_t server_thread;
+
+long queueId[1];
+
+long obtainQueue() {
+    struct msqid_ds buf;
+    long aux;
+    if((aux = msgget(MSQID, IPC_CREAT | 0x1B6)) < 0) {
+        printf("erro na criacao da fila %ld - errno: %d \n", aux, errno);
+        exit(1);
+    }
+
+    if(msgctl(int(aux), IPC_RMID, &buf) == -1) {
+        printf("Erro ao deletar p fila de mensagens\n");
+    }
+
+    if((aux = msgget(MSQID, IPC_CREAT|0x1B6|IPC_EXCL)) < 0) {
+        printf("erro na criacao da fila %ld - errno: %d \n", aux, errno);
+        exit(1);
+    }
+    return aux;
+}
+
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
-
-    return a.exec();
+    w.show();    
+    void* arg = NULL;
+    int val;
+    queueId[0] = obtainQueue();
+    pthread_create(&server_thread, NULL, server, arg);
+    auto ret = a.exec();
+    pthread_cancel(server_thread);
+    return ret;
 }
+
---
+++ ArachnID/server.cpp
@@ -0,0 +1,59 @@
+// Server side C/C++ program to demonstrate Socket programming
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include "pthread.h"
+#include <bits/stdc++.h>
+#include <QDebug>
+using namespace std;
+
+#define PORT 8080
+
+void* server(void*) {
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    string hello = "Hello from server";
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)  {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))  {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *) &address, sizeof(address))<0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    while(1) {
+
+        if (listen(server_fd, 3) < 0)  {
+            perror("listen");
+            exit(EXIT_FAILURE);
+        }
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+        QString s(buffer);
+        valread = read( new_socket , buffer, 2048);
+        qDebug() << s << endl;
+        // send(new_socket , hello , strlen(hello) , 0 );
+        // printf("Hello message sent\n");
+    }
+}
