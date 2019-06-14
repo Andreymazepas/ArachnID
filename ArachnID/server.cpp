6e75a5dc4d4acabe53ad4db59618a29deee3c5a2
gtaumaturgo <gtaumaturgo@github.com>
2019-06-14 01:44:54 -0300

    refactored the whole server code for readability

--- ArachnID/server.cpp
+++ ArachnID/server.cpp
@@ -14,102 +14,125 @@
 #include <netdb.h>
 using namespace std;
 
-#define PORT 8080
-void test(int joj, string s);
+void do_send(int joj, string s);
 
+void debug(string s) {
+    qDebug() << QString(s.c_str()) << endl;
+}
 
-void* server(void* arg) {
-    long queueId = *((long*)arg);
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[2048] = {0};
-    string hello = "Hello from server";
+int read_all_content_from_socket(int sock_fd,char* buf) {
+    recv(sock_fd,buf,1,0);
+    int read = 1;
+    while(recv(sock_fd,buf+read,1,MSG_DONTWAIT) > 0) {
+//        debug("eita " + to_string(read));
+        read++;
+    }
+    return read;
+}
 
-    // Creating socket file descriptor
+QString read_from_socket(int socket) {
+    char buffer[1 << 18];
+    read_all_content_from_socket(socket, buffer);
+    return QString(buffer);
+}
+
+int create_server_fd() {
+    int server_fd;
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)  {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
+    return server_fd;
+}
 
-    // Forcefully attaching socket to the port 8080
+// Forcefully attaching socket to the port 8080
+void attach_socket_to_port(int server_fd,struct sockaddr_in *address, int port = 8080) {
+    int opt = 1;
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))  {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
+    address->sin_family = AF_INET;
+    address->sin_addr.s_addr = INADDR_ANY;
+    address->sin_port = htons( port );
 
     // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr *) &address, sizeof(address))<0) {
+    if (bind(server_fd, (struct sockaddr *) address, sizeof(*address))<0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
-    while(1) {
+}
 
-        if (listen(server_fd, 3) < 0)  {
-            perror("listen");
-            exit(EXIT_FAILURE);
+
+string getHostFromHTTPRequest(string request) {
+    string target = "\r\nHost: ";
+    for(int i = 0; i < int(request.size()); i++) {
+        for(int j = i; j < int(target.size())+ i and j < int(request.size()); j++) {
+            if(target[j-i] != request[j]) break;
+            if(j == int(target.size()) + i - 1) {
+                string result = "";
+                for(int k = j + 1; k < int(request.size()) - 1; k++) {
+                    if(request[k] == '\r' and request[k+1] == '\n') {
+                        break;
+                    }
+                    result += request[k];
+                }
+
+                qDebug() << QString(result.c_str()) << endl;
+                return result;
+            }
         }
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+    }
+//    throw exception("kekek");
+}
+
+int send_request_to_the_web(string request) {
+    struct addrinfo hints, *res;
+    int web_sock_fd;
+    char buf[1 << 18];
+    long int byte_count;
+
+    //get host info, make socket and connect it
+    memset(&hints, 0,sizeof hints);
+    hints.ai_family=AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    int get_addr_res = getaddrinfo(getHostFromHTTPRequest(request).c_str(),"80", &hints, &res);
+    web_sock_fd = socket(res->ai_family,res->ai_socktype,res->ai_protocol);
+    connect(web_sock_fd,res->ai_addr,res->ai_addrlen);
+    send(web_sock_fd,request.c_str(),request.size(),0);
+    return web_sock_fd;
+}
+
+void send_response_to_browser(int browser_socket, QString response) { // ver esses tamanho
+    send(browser_socket , response.toStdString().c_str() , (unsigned long)response.size() , 0 );
+}
+
+void* server(void* arg) {
+    long queueId = *((long*)arg);
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    int server_fd = create_server_fd();
+    attach_socket_to_port(server_fd, &address);
+    if (listen(server_fd, 25) < 0)  {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    while(1) {
+        debug("======================================================");
+        int browser_socket;
+        if ((browser_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
             perror("accept");
             exit(EXIT_FAILURE);
         }
-<<<<<<< HEAD
-        valread = read( new_socket , buffer, 2048);
-        QString s(buffer);
-        qDebug() << s << endl;
 
-
-
-
-       test(new_socket, s.toStdString());
-=======
-        qDebug() << address.sin_family << endl;
-        qDebug() << address.sin_addr.s_addr << endl;
-        qDebug() << address.sin_port << endl;
-        qDebug() << new_socket << endl;
-        valread = read(new_socket , buffer, 2048);
-        QString s(buffer);
-        qDebug() << s << endl;
-        test(new_socket);
->>>>>>> 76a2f65ad8eb92c1142c39f2d5c8a87a19f59898
-
-        //send(new_socket ,  , 400 , 0 );
-        // printf("Hello message sent\n");
-        fflush(stdout);
-        close(new_socket);
+        QString request = read_from_socket(browser_socket);
+        qDebug() << request.toUtf8() << endl;
+        int web_sock_fd = send_request_to_the_web(request.toStdString());
+        QString response = read_from_socket(web_sock_fd);
+        send_response_to_browser(browser_socket, response);
+        close(web_sock_fd);
+        close(browser_socket);
     }
 }
 
-void test(int new_socket, string s) {
-    struct addrinfo hints, *res;
-        int sockfd;
-
-        char buf[20560];
-        long int byte_count;
-
-          //get host info, make socket and connect it
-          memset(&hints, 0,sizeof hints);
-          hints.ai_family=AF_UNSPEC;
-          hints.ai_socktype = SOCK_STREAM;
-          getaddrinfo("www.example.com","80", &hints, &res);
-          sockfd = socket(res->ai_family,res->ai_socktype,res->ai_protocol);
-          printf("Connecting...\n");
-          connect(sockfd,res->ai_addr,res->ai_addrlen);
-          printf("Connected!\n");
-          string header = s;
-          send(sockfd,header.c_str(),header.size(),0);
-          printf("GET Sent...\n");
-          //all right ! now that we're connected, we can receive some data!
-          byte_count = recv(sockfd,buf,sizeof(buf)-1,0); // <-- -1 to leave room for a null terminator
-          buf[byte_count] = 0; // <-- add the null terminator
-          printf("recv()'d %ld bytes of data in buf\n",byte_count);
-          printf("%s",buf);
-
-          send(new_socket , buf , (unsigned long)byte_count , 0 );
-          printf("acabou\n");
-          return;
-}
